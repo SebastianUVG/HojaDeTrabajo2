b7ed92c2a1ba679b00abc25fadbc7ec56aaad6b2
OscarEscriba <128449179+OscarEscriba@users.noreply.github.com>
2024-02-01 23:48:17 -0600

    Update CalculadoraPostfix.java

--- Calculadora/src/CalculadoraPostfix.java
+++ Calculadora/src/CalculadoraPostfix.java
@@ -7,7 +7,7 @@ public class CalculadoraPostfix {
     public static void main(String[] args) {
         try {
             
-            String rutaArchivo = "datos.txt";
+            String rutaArchivo = "src/datos.txt";
             BufferedReader br = new BufferedReader(new FileReader(rutaArchivo));
             String expresion = br.readLine();
             br.close();
@@ -16,12 +16,9 @@ public class CalculadoraPostfix {
                 System.out.println("El archivo está vacío o no contiene una expresión válida.");
                 return; // Sale del programa si no hay expresión válida.
             }
-
             POSFIXCalc calculadora = new POSFIXCalc();
             int resultadoFinal = calculadora.calcularExpresion(expresion);
-
             System.out.println("Resultado: " + resultadoFinal);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
